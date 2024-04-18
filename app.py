33333c09ea1b54d5bf8eb4f1f5a2fd030d85e779
Jessica Peixoto <jtpeixoto91@gmail.com>
2024-04-18 15:50:12 -0300

    Feat: Estudos com flask

--- app.py
+++ app.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, World!!"
+    fruta1 = "Morango"
+    fruta2 = "Maça"
+    fruta3 = "Laranja"
+    fruta4 = "Uva"
+    return render_template("index.html", 
+                           fruta1=fruta1, 
+                           fruta2=fruta2, 
+                           fruta3=fruta3, 
+                           fruta4=fruta4)
+
+@app.route("/sobre")
+def sobre():
+    return render_template("sobre.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
+
 
 #http://127.0.0.1:5000
